b7efcc7084a9392856d2dc423a6730080178e22d
M VAISHNAVI <160494652+Vaishmurugan@users.noreply.github.com>
2025-05-30 19:54:03 +0530

    Add files via upload

---
+++ vowel.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class secondassign {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a single alphabet character: ");
+        char inputChar = scanner.next().charAt(0);
+        char ch = Character.toLowerCase(inputChar);
+
+        if (ch >= 'a' && ch <= 'z') {
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                System.out.println("'" + inputChar + "' → Vowel");
+            } else {
+                System.out.println("'" + inputChar + "' → Consonant");
+            }
+        } else {
+            System.out.println("'" + inputChar + "' is not a valid alphabet character.");
+        }
+
+    }
+}
